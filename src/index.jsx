5c1156cab43813deee96de132808f12ce5c2dd07
platformprojmanagePRJG5 <platformprojmanagePRJG5@naicoits.com>
2023-07-11 01:58:05 +0530

    trying fix for two backend error

--- src/index.jsx
+++ src/index.jsx
@@ -54,7 +54,7 @@ class ReactFormBuilder extends React.Component {
       toolbarProps.items = this.props.toolbarItems;
     }
     return (
-      <DndProvider backend={HTML5Backend} key={1}>
+      <DndProvider backend={HTML5Backend} key={1} context={window}>
         <IntlProvider
           locale={currentAppLocale.locale}
           messages={currentAppLocale.messages}
